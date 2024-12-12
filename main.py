42428a67766b1ca90bc40364d122e4f4c8e77171
Lidiia Chernysh <lidiia.chernysh@uptech.team>
2024-12-12 23:12:49 +0100

    4th

--- main.py
+++ main.py
@@ -2,3 +2,18 @@ print("Hello world")
 print("Hello world")
 print("Hello world")
 print("Hello world")
+# Додайте виключення, щоб повідомити Python про ігнорування апострофа 
+name = "Wendy\'s dog"
+print(name)
+float1 = 5.0
+float2 = -1.25
+float3 = 587643291.001
+print(float1, float2, float3)
+
+# Змініть вхідні дані на ціле число
+age = int(input("How old are you now? " ))
+print("How old will you be next year?")
+print(age + 1)
+
+
+
